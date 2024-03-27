2a987325cb502e5b0a9a5aa92043e67f1757fff6
kick-depo <kaperlinkor@yandex.ru>
2024-03-28 00:26:16 +0300

    Смена расширений на .php, добавление хедера, сайдбара и футера

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer>
+  <div class="container">
+    <div class="row pt-4">
+      <div class="col-lg-3 col-sm-6 rini_footer">
+          <a class="text-uppercase d-block" href="cartridge.html">Картриджи</a>
+          <a class="text-uppercase d-block" href="office-equipment.html">Оргтехника</a>
+          <a class="text-uppercase d-block" href="print_service.html">Сервис печати</a>
+          <a class="text-uppercase d-block" href="zapravka/zapravka.html">Заправка картриджей</a>
+          <a class="text-uppercase d-block" href="paper.html">Бумага</a>
+          <a class="text-uppercase d-block" href="#">Скачать прайс</a>
+          <a class="text-uppercase d-block" href="contact.html">Контакт</a>
+      </div>
+      <div class="col-lg-3 col-sm-6 rini_footer">
+        <p class="text-muted text-uppercase mb-1">Телефоны</p>
+        <p class="mb-1">+7 (495) 649-64-49</p>
+        <p class="mb-3">+7 (903) 729-00-54</p>
+        <p class="text-muted text-uppercase mb-1">Почта</p>
+        <a href="mailto:6496449@rini.ru">6496449@rini.ru</a>
+        <p class="mb-1">ICQ: 6496449</p>
+      </div>
+      <div class="col-lg-3 col-sm-6 rini_footer">
+        <p class="text-muted text-uppercase mb-1">Режим работы</p>
+        <p>Режим работы в будни:<br>с 09:00 до 19:00</p>
+        <p>Режим работы в праздники:<br>с 12:00 до 16:00</p>
+      </div>
+      <div class="col-lg-3 col-sm-6 rini_footer">
+        <p class="text-muted text-uppercase mb-1">Адрес</p>
+        <p>Москва, Волгоградский<br>проспект, дом 28.</p>
+        <a title="Посмотреть адрес на карте (maps.yandex.ru)" href="http://maps.yandex.ru/?text=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D0%92%D0%BE%D0%BB%D0%B3%D0%BE%D0%B3%D1%80%D0%B0%D0%B4%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C%2028&amp;sll=37.682889%2C55.726107&amp;sspn=0.016457%2C0.006195&amp;ll=37.684434%2C55.726593&amp;spn=0.026141%2C0.008309&amp;l=sat" target="_blank" rel="nofollow">Посмотреть на карте</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p class="mb-1">© 2009 – 2024 <a href="http://rini.ru/">RINI.RU</a></p>
+        <p>Картриджи, расходные материалы для оргтехники. <a href="#">Политика обработки персональных данных</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<?php wp_footer(); ?>  
+</body>
+</html>
